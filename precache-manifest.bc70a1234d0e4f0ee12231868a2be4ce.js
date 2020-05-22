c5a05e52c3983e1747ead404c7d553725a7fd189
sphariab@gmail.com <sphariab@gmail.com>
2020-05-22 20:17:03 +0300

    Updates

---
+++ precache-manifest.bc70a1234d0e4f0ee12231868a2be4ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72631d4695f50ef2abd04d93933ec8e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a7334cfe9f2b58140ca2",
+    "url": "/tic-tac-toe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29a71728104aede14b88",
+    "url": "/tic-tac-toe/static/js/2.c2a201e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.c2a201e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7334cfe9f2b58140ca2",
+    "url": "/tic-tac-toe/static/js/main.f1335b8d.chunk.js"
+  },
+  {
+    "revision": "ecbc6f5089b996663329",
+    "url": "/tic-tac-toe/static/js/runtime-main.a982f7cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
